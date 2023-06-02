cff400dcef59a762d08172028599d90a18a0582c
Juhzh <2323257081@qq.com>
2023-06-02 15:38:13 +0800

    增加学院新闻页面

---
+++ news/js/newsload.js
@@ -0,0 +1,24 @@
+/* 模块加载函数 */
+function loadNewsModule(filepath) {
+    fetch(filepath)
+        .then(response => response.text())
+        .then(data => {
+            // 将filepath的内容插入到所有具有class为"news-module"的<div>中
+            const newsModules = document.getElementsByClassName('page1');
+            for (let i = 0; i < newsModules.length; i++) {
+                newsModules[i].innerHTML = data;
+            }
+        });
+}
+
+function loadDocumentModule(filepath) {
+    fetch(filepath)
+        .then(response => response.text())
+        .then(data => {
+            // 将filepath的内容插入到所有具有class为"news-module"的<div>中
+            const newsModules = document.getElementsByClassName('document1');
+            for (let i = 0; i < newsModules.length; i++) {
+                newsModules[i].innerHTML = data;
+            }
+        });
+}
